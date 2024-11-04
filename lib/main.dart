cc3687348d339250a2c7e1e93b4749fb310fd4c9
dungdai <tungmj1605@gmail.com>
2024-11-04 20:44:13 +0000

    a

---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:myapp/common/utils/app_routes.dart';
+import 'package:myapp/common/utils/environment.dart';
+import 'package:myapp/common/utils/kstrings.dart';
+import 'package:myapp/src/onboarding/controllers/onboarding_notifier.dart';
+import 'package:myapp/src/splashscreen/views/splashscreen_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: Environment.fileName);
+  await GetStorage.init();
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+        create: (_) => OnboardingNotifier(),
+      ),
+    ],
+    child: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    return ScreenUtilInit(
+      designSize: screenSize,
+      minTextAdapt: true,
+      splitScreenMode: false,
+      useInheritedMediaQuery: true,
+      builder: (_, child) {
+        return MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          title: AppText.kAppName,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          routerConfig: router,
+        );
+      },
+      child: const SplashScreen(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              Environment.apiKey,
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/src/onboarding/widgets/welcome_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:myapp/common/services/storage.dart';
+import 'package:myapp/common/utils/app_routes.dart';
+import 'package:myapp/common/utils/kcolors.dart';
+import 'package:myapp/common/utils/kstrings.dart';
+import 'package:myapp/common/widgets/app_style.dart';
+import 'package:myapp/common/widgets/custom_button.dart';
+import 'package:myapp/const/resource.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Kolors.kWhite,
+        width: ScreenUtil().screenWidth,
+        height: ScreenUtil().screenHeight,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20.h,
+            ),
+            Image.asset(
+              R.ASSETS_IMAGES_GETSTARTED_PNG,
+            ),
+            SizedBox(
+              height: 30.h,
+            ),
+            Text(
+              AppText.kWelcomeHeader,
+              textAlign: TextAlign.center,
+              style: appStyle(24, Kolors.kPrimary, FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20.h,
+            ),
+            SizedBox(
+              width: ScreenUtil().screenWidth - 100,
+              child: Text(
+                AppText.kWelcomeMessage,
+                textAlign: TextAlign.center,
+                style: appStyle(11, Kolors.kGray, FontWeight.normal),
+              ),
+            ),
+            GradientBtn(
+              text: AppText.kGetStarted,
+              btnHieght: 35,
+              radius: 20,
+              btnWidth: ScreenUtil().screenWidth - 100,
+              onTap: () {
+                ///TODO: uncomment the bool when app ready
+                // Storage().setBool('fisrtOpen', true);
+                context.go('/home');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
